0ad94389dee0725bf016b2a3b964c7aa022735df
Ivan Girotto <igirotto@ictp.it>
2023-09-05 21:40:46 +0200

    added 2nd version PI

--- Day2/lab/my_pi.c
+++ Day2/lab/my_pi.c
@@ -44,7 +44,7 @@ int main( int argc, char * argv[] )
   t1 = seconds();
   
   //  for( int i = start_i + 1 ; i <= end_i; i++ ) {
-  for( int i = rank ; i <= n; i+=size ) {
+  for( int i = rank + 1; i <= n; i+=size ) {
 
     x = w * (i - 0.5);
     f_x = 4.0 * 1.0 /(x*x + 1.0);
