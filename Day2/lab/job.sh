90a4dffaff65a2a249c4ad0fe2b05790a12c9f3e
Ivan Girotto <igirotto@ictp.it>
2023-09-05 21:21:12 +0200

    Update job.sh

--- Day2/lab/job.sh
+++ Day2/lab/job.sh
@@ -8,9 +8,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=490000MB
 
+#modulefiles to be loaded to have MPI on Leonardo
 module purge
 module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8
 
+#compiling command (valid also on your laptop)
 mpicc -O3 my_pi.c
 
+#running command (valid also on your laptop)
 mpirun -np 4 ./a.out
